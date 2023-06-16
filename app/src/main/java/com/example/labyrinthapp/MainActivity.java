853179e0efa4ecd89417260c0a84e8b6fcc03d11
Moritz Süssel <suessel.moritz@web.de>
2023-06-16 16:51:19 +0200

    App Navigations Struktur

--- app/src/main/java/com/example/labyrinthapp/MainActivity.java
+++ app/src/main/java/com/example/labyrinthapp/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.labyrinthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.Fragment;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
+    private enum ScreenEnum {
+        STARTSCREEN, GAMESCREEN
+    }
+    ScreenEnum screenEnum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, StartScreenFragment.class, null)
+                    .commit();
+        }
+        screenEnum = ScreenEnum.STARTSCREEN;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    public void onPlayButtonClick(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.fragment_container_view, GameScreenFragment.class, null)
+                .commit();
+        screenEnum = ScreenEnum.GAMESCREEN;
+    }
+
+    public void onHomeButtonClick(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.fragment_container_view, StartScreenFragment.class, null)
+                .commit();
+        screenEnum = ScreenEnum.STARTSCREEN;
+    }
+
+    public void onSettingsClick(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view, SettingsFragment.class, null)
+                .commit();
+    }
+
+    public void onCloseClick(View view) {
+        if(screenEnum == ScreenEnum.STARTSCREEN) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container_view, StartScreenFragment.class, null)
+                    .commit();
+        }
+        else {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container_view, GameScreenFragment.class, null)
+                    .commit();
+        }
+    }
+
+    public void onBestenlisteClick(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view, BestenlisteFragment.class, null)
+                .commit();
     }
 }
---
+++ app/src/main/java/com/example/labyrinthapp/StartScreenFragment.java
@@ -0,0 +1,19 @@
+package com.example.labyrinthapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class StartScreenFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_screen, container, false);
+    }
+}
